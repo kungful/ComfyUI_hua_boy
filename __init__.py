bd68c32db19e3b4d1aac231538b78bcabfa4e155
hua(Kungfu) <84950880+kungful@users.noreply.github.com>
2024-10-21 01:48:11 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,11 +6,11 @@ from .hua_word_models import Modelhua
 
 NODE_CLASS_MAPPINGS = {
     "ComfyUI_hua_boy": Huaword,
-    "ComfyUI_hua_boy": Modelhua
+    "小字体说明：我是comfyui_hua_boy的model": Modelhua
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "小字体说明：我是comfyui_hua_boy": "布尔图片Boolean_image",
+    "ComfyUI_hua_boy": "布尔图片Boolean_image",
     "小字体说明：我是comfyui_hua_boy的model": "布尔模型Boolean_model"
 }
 
